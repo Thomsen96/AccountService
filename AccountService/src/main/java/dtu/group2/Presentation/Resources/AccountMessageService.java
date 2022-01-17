f0e4ebc4233ac5110b47ed020256cb228984c0c2
Nicholas <36410886+GodOfAllBeings@users.noreply.github.com>
2022-01-17 14:21:26 +0100

    Create implemented with handler and request

--- AccountService/src/main/java/dtu/group2/Presentation/Resources/AccountMessageService.java
+++ AccountService/src/main/java/dtu/group2/Presentation/Resources/AccountMessageService.java
@@ -2,6 +2,7 @@ package dtu.group2.Presentation.Resources;
 
 import dtu.group2.Application.AccountServiceServer;
 import dtu.ws.fastmoney.Account;
+import dtu.ws.fastmoney.BankServiceException_Exception;
 import messaging.Event;
 import messaging.MessageQueue;
 
@@ -12,13 +13,35 @@ public class AccountMessageService {
     private MessageQueue messageQueue;
     
     private CompletableFuture<Account> accountPending;
-    private AccountServiceServer accountServiceServer;
+    private final AccountServiceServer accountServiceServer;
 
     public AccountMessageService(MessageQueue messageQueue, AccountServiceServer ass){
         this.messageQueue = messageQueue;
         this.accountServiceServer = ass;
         this.messageQueue.addHandler("GetCustomer", this::handleGetCustomer);
         this.messageQueue.addHandler("CustomerVerificationRequested", this::handleVerifyCustomer);
+    }
+
+    public void createCustomerAccountRequest(Event event) throws BankServiceException_Exception {
+        String customerId = event.getArgument(0, String.class);
+        String sessionId = event.getArgument(1, String.class);
+        accountServiceServer.createCustomer(customerId);
+        Event response = new Event("MerchantCreationResponse." + sessionId, new Object[]{customerId});;
+        if(accountServiceServer.getMerchant(customerId) == null) {
+            response = new Event("MerchantCreationResponse." + sessionId, new Object[]{"AN ERROR HAS OCCURED - COULD NOT CREATE CUSTOMER"});
+        }
+        messageQueue.publish(response);
+    }
+
+    public void createMerchantAccountRequest(Event event) throws BankServiceException_Exception {
+        String merchantId = event.getArgument(0, String.class);
+        String sessionId = event.getArgument(1, String.class);
+        accountServiceServer.createCustomer(merchantId);
+        Event response = new Event("MerchantCreationResponse." + sessionId, new Object[]{merchantId});;
+        if(accountServiceServer.getMerchant(merchantId) == null) {
+            response = new Event("MerchantCreationResponse." + sessionId, new Object[]{"AN ERROR HAS OCCURED - COULD NOT CREATE MERCHANT"});
+        }
+        messageQueue.publish(response);
     }
 
     public void handleVerifyCustomer(Event event) {
